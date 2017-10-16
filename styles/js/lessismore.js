5195b379f1281e635ebe79215155a9522db4b373
JQSC <122377305@qq.com>
2017-10-16 14:13:36 +0800

    git push

--- styles/js/lessismore.js
+++ styles/js/lessismore.js
@@ -61,7 +61,7 @@
 SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
-    json: '/search.json',
+    json: '/search.json' ,
     searchResultTemplate: '<li><a href="{url}">. {title}</a></li>',
     noResultsText: '',
     limit: 5,
